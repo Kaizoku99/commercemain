ea6f065326bd22041ee1431a94768a3e8e0bcdb7
Kaizoku99 <abdlrhman.saif@gmail.com>
2026-03-04 22:51:18 +0000

    fix: resolve Window.fbq type conflict in meta-pixel

--- components/analytics/meta-pixel.tsx
+++ components/analytics/meta-pixel.tsx
@@ -7,7 +7,9 @@ const PIXEL_ID = process.env.NEXT_PUBLIC_META_PIXEL_ID || '24439733515663379'
 
 declare global {
   interface Window {
-    fbq: (...args: any[]) => void
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    fbq: any
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
     _fbq: any
   }
 }
